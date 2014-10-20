ce6abe17d951081f43b9fb4a3fb6ccef7489311c
Yashprit Singh <yashprits@gmail.com>
2014-10-21 02:14:47 +0530

    added argument for image

--- bin/cli.js
+++ bin/cli.js
@@ -2,4 +2,10 @@
 'use strict';
 var GandhiSaid = require('../lib/gandhi.js');
 
+if(process.argv.length > 2){
+  if(process.argv[2] === "p" || process.argv[2] === "picture") {
+    console.log(GandhiSaid.renderAscii());
+  }
+}
+
 console.log(GandhiSaid.showQuotes());
